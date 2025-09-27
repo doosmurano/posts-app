042967e1ff5f5bc7a66b3f9337e1a00423c5e31e
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-09-27 20:47:49 +0300

    feat: Подготовит логику для переключения темы с помощью контекста

---
+++ src/shared/lib/theme/ThemeProvider.tsx
@@ -0,0 +1,37 @@
+import { createContext } from "react";
+import { FC, PropsWithChildren, useEffect, useState } from "react";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextType {
+    theme: Theme;
+    toggleTheme: () => void;
+}
+
+const LOCAL_STORAGE_KEY = "theme";
+
+export const ThemeContext = createContext<ThemeContextType>({
+    theme: "light",
+    toggleTheme: () => {},
+});
+
+export const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
+    const [theme, setTheme] = useState<Theme>(() => {
+        const savedTheme = localStorage.getItem(LOCAL_STORAGE_KEY);
+        return savedTheme as Theme || "light";
+    });
+    const toggleTheme = () => {
+        setTheme((current) => current === "light" ? "dark" : "light");
+    }
+
+    useEffect(() => {
+        document.body.setAttribute("data-theme", theme);
+        localStorage.setItem(LOCAL_STORAGE_KEY, theme);
+    }, [theme]);
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+} 
