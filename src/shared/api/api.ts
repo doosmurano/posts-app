465a8654713181532119fd0ee07cd7d3d3681d54
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-10-01 01:35:08 +0300

    feat: Создаст слайс API. Создаст хранилище store и обновит логику приложения с учётом хранилища

---
+++ src/shared/api/api.ts
@@ -0,0 +1,34 @@
+import { Post, Comment } from "../../types/api";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const postsApi = createApi({
+    reducerPath: "postsApi",
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: "https://jsonplaceholder.typicode.com" 
+    }),
+
+    tagTypes: ["Post", "Comment"],
+
+    endpoints: (builder) => ({
+        getPosts: builder.query<Post[], void>({
+            query: () => "posts",
+            providesTags: ["Post"],
+        }),
+
+        getPostById: builder.query<Post, number>({
+            query: (id) => `posts/${id}`,
+            providesTags: ( _, __, postId) => [
+                { type: "Post", id: postId },
+            ],
+        }),
+
+        getCommentsByPostId: builder.query<Comment[], number>({
+            query: (postId) => `posts/${postId}/comments`,
+            providesTags: ( _, __, postId) => [
+                { type: "Comment", id: postId },
+            ],
+        }),
+    }),
+})
+
+export const { useGetPostsQuery, useGetPostByIdQuery, useGetCommentsByPostIdQuery } = postsApi;
