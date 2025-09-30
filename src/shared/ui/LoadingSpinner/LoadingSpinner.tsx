fdf04843bd02cdd3ce496b420ec10adc88c8abb0
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-10-01 01:18:27 +0300

    feat: Создаст компонент LoadingSpinner

---
+++ src/shared/ui/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const LoadingSpinner: FC = () => {
+    return (
+        <div className="loading-spinner-wrapper">
+            <div className="loading-spinner"></div>
+        </div>
+    )
+}
