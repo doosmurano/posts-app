e4636481388dbb9948b54325ea8c8f5154ae7eec
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-10-06 17:19:21 +0300

    feat: Интегрирует фильтр по длине заголовка в PostList

--- src/widgets/PostList/PostList.tsx
+++ src/widgets/PostList/PostList.tsx
@@ -1,8 +1,11 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Post } from "../../types/api";
+import { PostLengthFilter } from "../../types/api";
 import { useGetPostsQuery } from "../../shared/api/api";
 import { PostCard } from "../../entities/post/ui/PostCard";
 import { withLoading } from "../../shared/lib/hoc/withLoading";
+import { filterByLength } from "../../features/PostLengthFilter/lib/filterByLength";
+import { PostLengthFilter as PostLengthFilterComponent } from "../../features/PostLengthFilter/ui/PostLengthFilter";
 
 const PostListDefault: FC<{posts: Post[]}> = ({ posts }) => {
   return (
@@ -17,18 +20,29 @@ const PostListDefault: FC<{posts: Post[]}> = ({ posts }) => {
 export const PostListWithLoading = withLoading(PostListDefault);
 
 export const PostList: FC = () => {
+const [filter, setFilter] = useState<PostLengthFilter>("все");
+
   const { data: posts, isLoading, isError, refetch } = useGetPostsQuery();
 
   const handleRetry = () => {
     refetch();
   };
 
+  const filteredPosts = filterByLength(posts  || [], filter);
+
     return (
-        <PostListWithLoading 
-          posts={posts || []} 
-          isLoading={isLoading} 
-          isError={isError} 
-          onRetry={handleRetry}
+      <>
+        <PostLengthFilterComponent
+        filter={filter}
+        onChange={setFilter}
         />
+        
+        <PostListWithLoading 
+        posts={filteredPosts} 
+        isLoading={isLoading} 
+        isError={isError} 
+        onRetry={handleRetry}
+        />
+      </>
     );
 }
