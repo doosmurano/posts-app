9f053ed6b880ddaf5cd0511c43f608c145f8d03d
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-10-07 16:38:33 +0300

    feat: Создаст компонент-страницу для роутера

---
+++ src/pages/PostPage/ui/PostsPage.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { PostList } from "../../../widgets/PostList/PostList";
+
+export const PostsPage: FC = () => {
+  return (
+      <div>
+          <PostList />
+      </div>
+  );
+}
