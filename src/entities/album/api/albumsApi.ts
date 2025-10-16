b9f83c9f957e10f12c6ad740173669f990acaf7a
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-10-17 01:58:52 +0300

    feat: Создаст логику для API постов, комментариев, альбомов, пользователей и задач

---
+++ src/entities/album/api/albumsApi.ts
@@ -0,0 +1,46 @@
+import { Album } from "@/types/api";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const albumsApi = createApi({
+    reducerPath: "albumsApi",
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: "https://jsonplaceholder.typicode.com" 
+    }),
+
+    tagTypes: ["Album"],
+
+    endpoints: (builder) => ({
+        getAlbums: builder.query<Album[], number>({
+            query: () => "albums",
+            providesTags: (result) =>
+                result
+            ?[
+                ...result.map(({ id }) => ({ type: "Album" as const, id })),
+                { type: "Album", id: "LIST"},
+            ]
+            : [{ type: "Album", id: "LIST" }],
+        }),
+
+        getAlbumById: builder.query<Album, number>({
+            query: (id) => `albums/${id}`,
+            providesTags: (_, __, id) => [{type: "Album", id}],
+        }),
+
+        getAlbumsByUserId: builder.query<Album[], number>({
+            query: (userId) => `albums?userId=${userId}`,
+            providesTags: (result, _, userId) =>
+               result
+            ?[
+                ...result.map(({ id }) => ({ type: "Album" as const, id })),
+                { type: "Album", id: `USER_${userId}` },
+            ]
+            : [{ type: "Album", id: `USER_${userId}` }],
+        }),
+    }),
+})
+
+export const { 
+    useGetAlbumsQuery, 
+    useGetAlbumByIdQuery, 
+    useGetAlbumsByUserIdQuery 
+} = albumsApi;
---
+++ src/entities/user/api/usersApi.ts
@@ -0,0 +1,34 @@
+import { User } from "@/types/api";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const usersApi = createApi({
+    reducerPath: "usersApi",
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: "https://jsonplaceholder.typicode.com" 
+    }),
+
+    tagTypes: ["User"],
+
+    endpoints: (builder) => ({
+        getUsers: builder.query<User[], number>({
+            query: () => "users",
+            providesTags: (result) =>
+                result
+            ?[
+                ...result.map(({ id }) => ({ type: "User" as const, id })),
+                { type: "User", id: "LIST"},
+            ]
+            : [{ type: "User", id: "LIST" }],
+        }),
+
+        getUserById: builder.query<User, number>({
+            query: (id) => `users/${id}`,
+            providesTags: (_, __, id) => [{type: "User", id}],
+        }),
+    }),
+})
+
+export const { 
+    useGetUsersQuery, 
+    useGetUserByIdQuery 
+} = usersApi;
