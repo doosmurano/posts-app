d258d39015d676eb1df6b9a66fe8a026b1411b21
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2025-10-17 15:58:48 +0300

    feat: Добавит логику отображения постов отдельного пользователя

--- src/pages/UserPostsPage/ui/UserPostsPage.tsx
+++ src/pages/UserPostsPage/ui/UserPostsPage.tsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import { Error } from "@/shared/ui/Error/Error";
+import { PostCard } from "@/entities/post/ui/PostCard";
 import { UserTabs } from "@/widgets/UserTabs/ui/UserTabs";
+import { useGetPostsByUserIdQuery } from "@/entities/post/api/postApi";
+import { LoadingSpinner } from "@/shared/ui/LoadingSpinner/LoadingSpinner";
 
 export const UserPostsPage = () => {
     const {id} = useParams();
@@ -10,10 +13,26 @@ export const UserPostsPage = () => {
         return <Error errorMessage="Некорректный ID пользователя" />;
     }
 
+    const {data: posts, isLoading, isError, refetch} = useGetPostsByUserIdQuery(userId);
+
     return (
         <div>
             <UserTabs userId={userId} />
-            <h1>Посты пользователя {userId}</h1>
+            <h2>Посты пользователя {userId}</h2>
+            {isLoading && <LoadingSpinner />}
+            {isError && <Error errorMessage="Ошибка загрузки постов" onRetry={refetch} />}
+            
+            {posts && posts.length === 0 && (
+                <p>У пользователя нет постов</p>
+            )}
+            
+            {posts && (
+                <div>
+                    {posts.map((post) => (
+                        <PostCard key={post.id} post={post} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
